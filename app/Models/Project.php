090d1acf9101165ecb30f15714714ec77ac94f4d
NicholasConti <conti94@live.it>
2023-05-25 18:57:28 +0200

    add accessor/mutator

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -19,5 +20,12 @@ class Project extends Model
     public function technologies()
     {
         return $this->belongsToMany(Technology::class);
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string|null $value) => $value !== null ?  asset('storage/' . $value) : null
+        );
     }
 }
